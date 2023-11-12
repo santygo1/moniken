7f0316964bc365605900a63c607589918bdf2274
Danil <danilkaspirin@gmail.com>
2023-11-13 09:01:50 +1000

    init commit

---
+++ backend/src/main/java/ru/moniken/MonikenApplication.java
@@ -0,0 +1,13 @@
+package ru.moniken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonikenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonikenApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ru/moniken/MonikenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moniken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonikenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
